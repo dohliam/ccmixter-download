af3ca9fdea5e4b925664e84fb8b5c963ef30b8dd
dohliam <dohliam@gmail.com>
2016-06-16 00:31:48 -0700

    refactor to allow for combining search terms

--- README.md
+++ README.md
@@ -8,60 +8,113 @@ There is a lot of great open-licensed music on [ccMixter](http://ccmixter.org/),
 
 You might want to rename the script to `ccm` and place it in your PATH. This will allow you to do for example:
 
-    ccm -s F_Fact
+    ccm -su F_Fact
 
-Which will stream a playlist of all tracks uploaded by user [F_Fact](http://ccmixter.org/people/F_Fact/profile).
+Which will begin streaming a playlist of all tracks uploaded by user [F_Fact](http://ccmixter.org/people/F_Fact/profile).
 
-The examples below will assume that you have renamed the executable to `ccm`.
+For the sake of convenience, the examples below will assume that you have renamed the executable to `ccm`.
 
 ## Options
-* `-d` (`--download`): _Download all tracks_
-* `-f` (`--save-to-file`): _Save urls to tracklist file_
-* `-h` (`--help`): _Show help_
-* `-l` (`--limit NUMBER`): _Specify results limit (default 200)_
-* `-m` (`--markdown`): _Print out playlist in markdown format with links_
-* `-p` (`--print`): _Print tracklist_
-* `-q` (`--query`): _Search for a keyword_
-* `-r` (`--raw`): _Output raw track array values (debugging)_
-* `-s` (`--stream`): _Stream entire playlist (requires mplayer)_
-* `-t` (`--tag`): _Specify tag instead of artist name_
+
+Options can be used individually or combined together to produce more fine grained results. The following options are available:
+
+* `-c`, (`--license LICENSE`): _Filter tracks by license_
+* `-d`, (`--download`): _Download all tracks_
+* `-f`, (`--save-to-file`): _Save urls to tracklist file_
+* `-l`, (`--limit NUMBER`): _Specify results limit (default 200)_
+* `-m`, (`--markdown`): _Print out playlist in markdown format with links_
+* `-p`, (`--print`): _Print tracklist_
+* `-q`, (`--query KEYWORD`): _Search for a keyword_
+* `-r`, (`--recommended`): _Sort by highest recommended uploads_
+* `-s`, (`--stream`): _Stream entire playlist (requires mplayer)_
+* `-t`, (`--tag TAG`): _Specify tag name_
+* `-u`, (`--user USER`): _Specify user name_
+* `-w`, (`--raw`): _Output raw track array values (debugging)_
+
+### Users
+
+To get all tracks uploaded by a particular user, use the `-u` option, for example:
+
+    ccm -u teru
+
+The above command will print out a list of tracks uploaded by user `teru`.
+
+This can be combined with other options to refine the list of results to a particular tag (`-t`) or sort according to number of recommendations (`-r`), as well as save the results to a playlist (`-s`) or download all files to disk (`-d`). Further examples are below.
 
 ### Tags
 
-By default using `ccm` and a search term will give results for a specific artist. To show results for a tag instead you can use the `-t` option.
+To get results for a specific tag you can use the `-t` option. This option can be used together with `-d`, `-f`, `p`, `-r`, and `-s` to download, save, print, output raw values or stream music based on a specific tag. For example, to stream a list of tracks tagged as "ambient", you could use:
 
-The `-t` option to use tags can be used together with `-d`, `-f`, `p`, `-r`, and `-s` to download, save, print, output raw values or stream music based on a specific tag. For example, to stream a list of tracks tagged as "ambient", you could use:
-
-    ccm -ts ambient
+    ccm -st ambient
 
 To download the tracks instead, use:
 
-    ccm -td ambient
+    ccm -dt ambient
+
+You can specify multiple tags by separating them with a space or a `+`:
+
+    ccm -t "celtic+electro+remix"
+
+    ccm -t "funk jazz saxophone chill trip_hop"
+
+### Keyword search
+
+You can search for an exact phrase, term, or title in track metadata using the `-q` option:
+
+    ccm -q "Violet Fusion"
+
+As always, this can be combined with other options to refine the search:
+
+    ccm -u grapes -q dunno
 
 ### Limiting results
 
 The default number of results provided by the ccMixter API is 200. However, many tags (such as `rock`, `hip_hop` or `techno`) and some artists (such as `Javolenus`) can have many more results. In such cases, you can raise the limit using the limit option (`-l`), for example to get the first 500 results from the list of tracks tagged as "techno":
 
-    ccm -tl 500 techno
+    ccm -l 500 -t techno
 
-To get the first 300 tracks by Javolenus:
+Similarly, to get the first 300 tracks by Javolenus:
 
-    ccm -l 300 Javolenus
+    ccm -l 300 -u Javolenus
+
+### Filter by license
+
+You can use the license option (`-c`) to filter the results for a given search by license. Only tracks with the specified license will be returned.
+
+For example, to display a list of tracks tagged as "blues" that are released under a CC-BY license, you can use:
+
+    ccm -t blues -c by
+
+The possibe license values are:
+
+Value | License name
+----- | ------------
+`by` | Attribution (CC BY)
+`nc` | NonCommercial (CC BY-NC)
+`sa` | ShareAlike (CC BY-SA)
+`nod` | NoDerivs (CC BY-ND)
+`byncsa` | NonCommercial ShareAlike (CC BY-NC-SA)
+`byncnd` | Attribution-NonCommercial-NoDerivs (CC BY-NC-ND)
+`s` | Sampling
+`splus` | Sampling+
+`ncsplus` | NonCommercial Sampling+
+`pd` | Public Domain (CC 0)
 
 ### Output format
 
+The default output format (e.g. for the `-p` or `-f` options) is a plain text list of download urls, separated by line breaks. However other output formats (e.g. markdown, raw) are also available.
+
 As with tags, the `-m` option can be combined with other options such as `-p`, `-t`, `-l`, and `-q` to produce nicely formatted results in markdown. For example, to get a markdown list of 300 tracks with the tag "jazz", use:
 
-    ccm -tml 300 jazz
+    ccm -ml 300 -t jazz
+
+For debugging purposes, the `-w` option outputs the track list in raw format (i.e., as an array of strings).
 
 ## To do
 
-* Filter by license
 * Search for remixes of a specific track
 * Play random track
 * Specify date range
-* Sort by highest recommended uploads
-* Combine multiple search terms
 
 ## License
 
--- ccmixter_download.rb
+++ ccmixter_download.rb
@@ -8,144 +8,93 @@ options = {}
 OptionParser.new do |opts|
   opts.banner = "\n  ==ccmixter-download - a tool for batch downloading and streaming songs from ccMixter==\n  Usage: ccmixter_download_artist.rb [ARTIST] [OPTIONS]"
 
+  opts.on("-c", "--license LICENSE", "Filter tracks by license") { |v| options[:license] = v }
   opts.on("-d", "--download", "Download all tracks") { options[:download] = true }
   opts.on("-f", "--save-to-file", "Save urls to tracklist file") { options[:save] = true }
   opts.on("-l", "--limit NUMBER", "Specify results limit (default 200)") { |v| options[:limit] = v }
   opts.on("-m", "--markdown", "Print out playlist in markdown format with links") { options[:markdown] = true }
   opts.on("-p", "--print", "Print tracklist") { options[:print] = true }
-  opts.on("-q", "--query", "Search for a keyword") { options[:search] = true }
-  opts.on("-r", "--raw", "Output raw track array values (debugging)") { options[:raw] = true }
+  opts.on("-q", "--query KEYWORD", "Search for a keyword") { |v| options[:search] = v }
+  opts.on("-r", "--recommended", "Sort by highest recommended uploads") { options[:recommended] = true }
   opts.on("-s", "--stream", "Stream entire playlist (requires mplayer)") { options[:stream] = true }
-  opts.on("-t", "--tag", "Specify tag instead of artist name") { options[:tag] = true }
+  opts.on("-t", "--tag TAG", "Specify tag name") { |v| options[:tag] = v }
+  opts.on("-u", "--user USER", "Specify user name") { |v| options[:user] = v }
+  opts.on("-w", "--raw", "Output raw track array values (debugging)") { options[:raw] = true }
 
 end.parse!
 
-def get_mp3_list(artist)
-  url = "http://ccmixter.org/api/query?f=html&t=links_by_dl_ul&u=#{artist}"
-
-  if @limit
-    url = "http://ccmixter.org/api/query?f=html&t=links_by_dl_ul&u=#{artist}&limit=#{@limit}"
-  end
-
-  content = open(url).read
-
-  titles = content.scan(/class="cc_file_link">(.*)<\/a>/)
-  mp3 = content.scan(/<a href="(http:\/\/ccmixter.org\/content\/#{artist}\/.*?)">/)
-
-  mp3
+def get_url_content(params)
+  url = "http://ccmixter.org/api/query?f=html&t=links_by_dl_ul" + params
+  open(URI.encode(url)).read
 end
 
-def download_all_tracks(term)
-  mp3 = []
+def get_track_list(params)
+  content = get_url_content(params)
+  content.scan(/<a href="(http:\/\/ccmixter.org\/content\/.*?)">/)
+end
 
-  if @tag
-    mp3 = get_tag_list(term)
-  else
-    mp3 = get_mp3_list(term)
-  end
+def download_all_tracks(params, descriptor)
+  mp3 = get_track_list(params)
 
   download_count = 1
   puts "  ** Fetching remote mp3s..."
   mp3.uniq.each do |m|
     filename = m[0].gsub(/.*\//, "")
-    FileUtils.mkdir_p term
+    FileUtils.mkdir_p descriptor
     progress = download_count.to_f / mp3.uniq.length.to_f * 100
-    File.write(term + "/" + filename, open(m[0]).read, {mode: 'wb'})
-    puts "  ##{download_count.to_s} of #{mp3.uniq.length.to_s}: #{filename} saved to #{term} directory! (#{progress.round(2).to_s}%)"
+    File.write(descriptor + "/" + filename, open(m[0]).read, {mode: 'wb'})
+    puts "  ##{download_count.to_s} of #{mp3.uniq.length.to_s}: #{filename} saved to #{descriptor} directory! (#{progress.round(2).to_s}%)"
     download_count += 1
   end
   puts "  ** All files saved to download folder!"
 end
 
-def tracklist_to_file(term, filename)
-  mp3 = []
-
-  if @tag
-    mp3 = get_tag_list(term)
-  elsif @search
-    mp3 = get_search_list(term)
-  else
-    mp3 = get_mp3_list(term)
-  end
+def tracklist_to_file(params, filename)
+  mp3 = get_track_list(params)
 
   mp3.uniq.each do |m|
     File.open(filename, "w") { |f| f << mp3.join("\n") }
   end
 end
 
-def save_tracklist(term)
-  filename = "tracklist_#{term}_ccmixter.txt"
-  tracklist_to_file(term, filename)
+def save_tracklist(params, basename)
+  filename = basename + ".txt"
+  tracklist_to_file(params, filename)
   puts "  ** Tracklist saved to #{filename}!"
 end
 
-def stream_playlist(term)
-  filename = "/tmp/ccmixter_#{term}.tmp"
+def stream_playlist(params, basename)
+  filename = "/tmp/" + basename + ".tmp"
 
-  tracklist_to_file(term, filename)
+  tracklist_to_file(params, filename)
 
   exec("mplayer -playlist #{filename}")
 end
 
-def print_tracklist(term)
-  mp3 = []
-
-  if @tag
-    mp3 = get_tag_list(term)
-  else
-    mp3 = get_mp3_list(term)
-  end
+def print_tracklist(params)
+  mp3 = get_track_list(params)
 
   mp3.uniq.each do |m|
     puts m
   end
 end
 
-def raw_tracklist(term)
-  mp3 = []
-
-  if @tag
-    mp3 = get_tag_list(term)
-  else
-    mp3 = get_mp3_list(term)
-  end
+def raw_tracklist(params)
+  mp3 = get_track_list(params)
 
   mp3.uniq.each do |m|
     p m
   end
 end
 
-def get_tag_list(tag)
-  url = "http://ccmixter.org/api/query?tags=#{tag}&f=html&t=links_by_dl_ul"
+def get_track_info(params)
+  content = get_url_content(params)
 
-  if @limit
-    url = "http://ccmixter.org/api/query?tags=#{tag}&f=html&t=links_by_dl_ul&limit=#{@limit}"
-  end
-
-  content = open(url).read
-
-  mp3 = content.scan(/<a href="(http:\/\/ccmixter.org\/content\/.*?)">/)
-
-  mp3
+  content.scan(/^\s+<li>\n\s+<a href="(http:\/\/ccmixter.org\/files\/.*?\/.*?)" class="cc_file_link">(.*?)<\/a>by     <a href="(http:\/\/ccmixter.org\/people\/.*?)">(.*?)<\/a>\n\s+<a href="(http:\/\/ccmixter.org\/content\/.*?)">(.*?)<\/a>\n\s+<\/li>/)
 end
 
-def print_markdown(term)
-  limit = ""
-  if @limit
-    limit = "&limit=#{@limit}"
-  end
-
-  url = "http://ccmixter.org/api/query?f=html&t=links_by_dl_ul&u=#{term}" + limit
-
-  if @tag
-    url = "http://ccmixter.org/api/query?tags=#{term}&f=html&t=links_by_dl_ul" + limit
-  elsif @search
-    url = "http://ccmixter.org/api/query?search=#{term}&f=html&t=links_by_dl_ul" + limit
-  end
-
-  content = open(url).read
-
-  info = content.scan(/^\s+<li>\n\s+<a href="(http:\/\/ccmixter.org\/files\/.*?\/.*?)" class="cc_file_link">(.*?)<\/a>by     <a href="(http:\/\/ccmixter.org\/people\/.*?)">(.*?)<\/a>\n\s+<a href="(http:\/\/ccmixter.org\/content\/.*?)">(.*?)<\/a>\n\s+<\/li>/)
+def print_markdown(params)
+  info = get_track_info(params)
 
   info.each do |i|
     ccmixter_link = i[0]
@@ -159,55 +108,76 @@ def print_markdown(term)
   end
 end
 
-def get_search_list(keyword)
-  url = "http://ccmixter.org/api/query?search=#{keyword}&f=html&t=links_by_dl_ul"
+def get_descriptor(desc_array)
+  desc = "untitled"
+  join = desc_array.join("@").gsub(/(^@+)|(@+$)/, "").gsub(/@+/, "_").gsub(/ /, "-")
 
-  if @limit
-    url = "http://ccmixter.org/api/query?search=#{keyword}&f=html&t=links_by_dl_ul&limit=#{@limit}"
+  if join != ""
+    desc = join
   end
-
-  content = open(url).read
-
-  mp3 = content.scan(/<a href="(http:\/\/ccmixter.org\/content\/.*?)">/)
-
-  mp3
+  desc
 end
 
-term = ""
+params = ""
+user = ""
+tag = ""
+search = ""
+license = ""
+limit = ""
+sort = ""
+desc_arr = ["", "", "", ""]
 
-if ARGV[0]
-  term = URI.encode(ARGV[0])
-else
-  puts "  ** No argument specified. Please use the -h option for help"
-  exit
+if options[:limit]
+  limit = "&limit=" + options[:limit]
+end
+
+if options[:recommended]
+  sort = "&sort=num_scores"
+end
+
+if options[:user]
+  user = "&u=" + options[:user]
+  desc_arr[0] = options[:user]
 end
 
 if options[:tag]
-  @tag = true
-end
-
-if options[:limit]
-  @limit = options[:limit]
+  tag = "&tags=" + options[:tag]
+  desc_arr[1] = options[:tag]
 end
 
 if options[:search]
-  @search = true
+  kw = options[:search]
+  search = "&search=" + kw.gsub(/ /, "+") + "&search_type=match"
+  desc_arr[2] = kw
+end
+
+if options[:license]
+  license = "&lic=" + options[:license]
+  desc_arr[3] = options[:license]
+end
+
+descriptor = get_descriptor(desc_arr)
+basename = descriptor + "_ccmixter_tracklist"
+
+params = user + tag + search + license + limit + sort
+
+if params == ""
+  puts "  ** No arguments specified. Please use the -h option for help."
+  exit
 end
 
 if options[:download]
-  download_all_tracks(term)
+  download_all_tracks(params, basename)
 elsif options[:save]
-  save_tracklist(term)
+  save_tracklist(params, basename)
 elsif options[:print]
-  print_tracklist(term)
+  print_tracklist(params)
 elsif options[:markdown]
-  print_markdown(term)
+  print_markdown(params)
 elsif options[:stream]
-  stream_playlist(term)
+  stream_playlist(params, basename)
 elsif options[:raw]
-  raw_tracklist(term)
-elsif options[:search]
-  puts get_search_list(term)
+  raw_tracklist(params)
 else
-  puts @tag ? get_tag_list(term) : get_mp3_list(term)
+  puts get_track_list(params)
 end
